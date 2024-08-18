e3a9db2d7dc77962a09a3d5a00f146aa1ae1d8af
Zainab Jafari <zainabjafari998@gmail.com>
2024-08-18 12:12:41 +0200

    deploy

--- api/index.js
+++ api/index.js
@@ -4,10 +4,11 @@ import multer from 'multer';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
-import commentRoutes from './routes/comments.js'
-import likesRoutes from './routes/likes.js'
-import relationshipRoutes from './routes/relationshipt.js'
+import commentRoutes from './routes/comments.js';
+import likesRoutes from './routes/likes.js';
+import relationshipRoutes from './routes/relationshipt.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
 
@@ -18,14 +19,14 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // Ändra detta till frontend-URL:en vid deployment
     credentials: true
 }));
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../client/public/upload');
+      cb(null, '../client/public/upload'); // Verifiera sökvägen beroende på din projektstruktur
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + file.originalname);
@@ -42,12 +43,21 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200).json(file.filename);
 });
 
+// API-routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/likes', likesRoutes);
 app.use('/api/relationships', relationshipRoutes);
+
+// Serva frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'frontend/build'))); // Förutsätter att 'frontend' är i samma katalog som 'backend'
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 app.listen(8000, () => {
     console.log('API working on port 8000');
