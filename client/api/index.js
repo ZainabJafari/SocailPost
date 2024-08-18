5f462690e060bcd6b29998d316b5dd69fa44fd11
Zainab Jafari <zainabjafari998@gmail.com>
2024-08-18 13:25:22 +0200

    deploy1

--- client/api/index.js
+++ client/api/index.js
@@ -56,7 +56,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'frontend/build'))); // Förutsätter att 'frontend' är i samma katalog som 'backend'
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(8000, () => {
